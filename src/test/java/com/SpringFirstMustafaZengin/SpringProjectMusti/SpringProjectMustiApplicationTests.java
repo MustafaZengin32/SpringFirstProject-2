0247c23491e3ee2b49d12f845cf875fdf7cc743a
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2024-12-31 22:26:35 +0100

    last

--- src/test/java/com/SpringFirstMustafaZengin/SpringProjectMusti/SpringProjectMustiApplicationTests.java
+++ src/test/java/com/SpringFirstMustafaZengin/SpringProjectMusti/SpringProjectMustiApplicationTests.java
@@ -1,13 +1,36 @@
 package com.SpringFirstMustafaZengin.SpringProjectMusti;
 
+import com.mustafazengin.dto.DtoCustomer;
+import com.mustafazengin.services.ICustomerService;
+import com.mustafazengin.services.IEmployeeService;
+import com.mustafazengin.starter.SpringProjectMustiApplication;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {SpringProjectMustiApplication.class})
 class SpringProjectMustiApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private ICustomerService iCustomerService;
+
+
+	@Test //test ler hep void doner
+	public void testFindCustomerById() {
+
+		DtoCustomer customer = iCustomerService.findCustomerById(1L);
+
+		if (customer!=null){
+
+			System.out.println("Isim "+customer.getName());
+		}
+
+		Assertions.assertEquals("Enes",customer.getName());
+
+
+
+
 	}
 
 }
